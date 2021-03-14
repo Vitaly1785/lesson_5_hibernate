5caa2cf7cf870d77cabb35f5e592b6844ffe6dd0
Vitaly1785 <korsar1785@yandex.ru>
2021-03-14 17:01:42 +0300

    С заданием справился. Подключал MySQL.

--- src/main/java/ru/geekbrains/Application.java
+++ src/main/java/ru/geekbrains/Application.java
@@ -1,7 +1,14 @@
 package ru.geekbrains;
 
+import ru.geekbrains.product.Product;
+import ru.geekbrains.productDao.ProductDao;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello");
+        ProductDao productDao = new ProductDao();
+        System.out.println(productDao.findById(1L).toString());
+        productDao.findAll().forEach(product -> System.out.println(product.toString()));
+//        productDao.delete(8L);
+        productDao.saveOrUpdate(new Product("Samsung", 500));
     }
 }
